074b52624093ee9bab66def8b8fa8cac4b47b2a9
asmae <asmae.imazzi@edu.uiz.ac.ma>
2022-02-26 16:32:47 +0100

    fizzbuzz kata tdd

--- src/main/java/katas/java/fizz_buzz/FizzBuzz.java
+++ src/main/java/katas/java/fizz_buzz/FizzBuzz.java
@@ -1,4 +1,13 @@
 package katas.java.fizz_buzz;
 
 public class FizzBuzz {
+
+    public static String fizzBuzz(int number) {
+        String result = "";
+        if(number%3 == 0)
+            result = "fizz";
+        if(number%5 == 0)
+            result += "buzz";
+        return result;
+    }
 }
--- src/test/java/katas/java/fizz_buzz/FizzBuzzTest.java
+++ src/test/java/katas/java/fizz_buzz/FizzBuzzTest.java
@@ -1,4 +1,33 @@
 package katas.java.fizz_buzz;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 public class FizzBuzzTest {
+
+    @Test
+    void should_return_fizz_when_number_is_multiple_of_three(){
+        assertEquals("fizz",FizzBuzz.fizzBuzz(3));
+    }
+
+    @Test
+    void should_return_empty_string_when_number_is_not_multiple_of_three_nor_of_five(){
+        assertEquals("",FizzBuzz.fizzBuzz(2));
+    }
+
+    @Test
+    void should_return_buzz_when_number_is_multiple_of_five(){
+        assertEquals("buzz",FizzBuzz.fizzBuzz(5));
+    }
+
+    @Test
+    void should_return_fizz_buzz_when_number_is_multiple_of_five_and_three(){
+        assertEquals("fizzbuzz",FizzBuzz.fizzBuzz(15));
+    }
+
+
+
 }
+
